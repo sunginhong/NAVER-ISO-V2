d966c80372a0dea1bccc90470b9e1a1652e76ad0
user <nicehsi@naver.com>
2022-05-27 17:15:34 +0900

    220527_2

--- app/src/main/java/com/example/naver_iso_v2/Fragment_4_ViewPager.java
+++ app/src/main/java/com/example/naver_iso_v2/Fragment_4_ViewPager.java
@@ -13,6 +13,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.CompositePageTransformer;
@@ -49,7 +50,9 @@ public class Fragment_4_ViewPager extends Fragment {
         viewPager.setAdapter(pagerAdapter);
         viewPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
         viewPager.setOffscreenPageLimit(3);
-        viewPager.setCurrentItem(1);
+        viewPager.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+        viewPager.setCurrentItem(1, false);
+        viewPager.setUserInputEnabled(true);
         this.initializeData();
 
         sliderRunnable = new Runnable() {
@@ -65,7 +68,7 @@ public class Fragment_4_ViewPager extends Fragment {
             @Override
             public void transformPage(@NonNull View page, float position) {
                 float r = 1 - Math.abs(position);
-                page.setScaleY(0.85f + r * 0.15f);
+//                page.setScaleY(0.85f + r * 0.15f);
             }
         });
         viewPager.setPageTransformer(compositePageTransformer);
@@ -113,7 +116,7 @@ public class Fragment_4_ViewPager extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        sliderHandler.postDelayed(sliderRunnable, 2000);
+//        sliderHandler.postDelayed(sliderRunnable, 2000);
     }
 
     @Override
